4d7f2b6f6372418bffb055056dfb7fc66d4c2fd4
Santiago Gasparotti <santiago.gasparotti@hotmail.com>
2021-07-21 21:34:52 -0300

    add js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let persona = "SANTIAGO GASPAROTTI"
+let domicilio = "ARTURO ALIO 1234"
+let pais = "ARG"
+let nacimiento = "05/11/1996"
+const codigo = "39873433";
+
+let carnet = "Codigo: "+codigo+ " "+
+            "Nombre: "+persona+ " "+
+            "Pais: "+pais+ " "+
+            "Nacimiento: "+nacimiento+ " "+
+            "Domicilio: "+domicilio;
+
+console.log(carnet)
