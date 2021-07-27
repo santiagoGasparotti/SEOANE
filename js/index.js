b943becf1c80101b265ff6671966eb148ffe4fce
Santiago Gasparotti <santiago.gasparotti@hotmail.com>
2021-07-27 17:25:08 -0300

    add ciclo

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-// let persona = "SANTIAGO GASPAROTTI"
-// let domicilio = "ARTURO ALIO 1234"
-// let pais = "ARG"
-// let nacimiento = "05/11/1996"
-// const codigo = "39873433";
+let persona = "SANTIAGO GASPAROTTI"
+let domicilio = "ARTURO ALIO 1234"
+let pais = "ARG"
+let nacimiento = "05/11/1996"
+const codigo = "39873433";
 
-// let carnet = "Codigo: "+codigo+ " "+
-//             "Nombre: "+persona+ " "+
-//             "Pais: "+pais+ " "+
-//             "Nacimiento: "+nacimiento+ " "+
-//             "Domicilio: "+domicilio;
+let carnet = "Codigo: "+codigo+ " "+
+            "Nombre: "+persona+ " "+
+            "Pais: "+pais+ " "+
+            "Nacimiento: "+nacimiento+ " "+
+            "Domicilio: "+domicilio;
 
-// console.log(carnet)
+console.log(carnet)
 
 
 // let registroUsuario = prompt("ingresa nombre de usuario");
